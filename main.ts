3ca478f14b2ace30656c988d575f2506cc5bc9f0
vGriimy <167124883+vGriimy@users.noreply.github.com>
2024-04-15 13:14:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+scene.setBackgroundColor(11)
+controller.moveSprite(mySprite)
