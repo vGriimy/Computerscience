f3aa98bc04ee8e5c99f4f9094e20954c9e4b8356
ralph64barcenas <167125106+ralph64barcenas@users.noreply.github.com>
2024-04-15 13:21:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,4 +17,25 @@ let mySprite = sprites.create(img`
     . . . . f f f f f f f f f . . . 
     `, SpriteKind.Player)
 scene.setBackgroundColor(11)
-controller.moveSprite(mySprite)
+controller.player1.moveSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    . . . c c c c c c . . . . . . . 
+    . . c 6 7 7 7 7 6 c . . . . . . 
+    . c 7 7 7 7 7 7 7 7 c . . . . . 
+    c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+    c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+    f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+    f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+    . f 7 7 7 7 6 c 7 7 6 f . . . . 
+    . . f c c c c 7 7 6 f c c c . . 
+    . . c 6 2 7 7 7 f c c 7 7 7 c . 
+    . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+    . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+    . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+    . . c 6 1 1 1 1 1 7 6 6 c c . . 
+    . . . c c c c c c c c c c . . . 
+    `, SpriteKind.Player)
+controller.player2.moveSprite(mySprite2)
+mySprite.setPosition(119, 47)
+mySprite2.setPosition(44, 53)
